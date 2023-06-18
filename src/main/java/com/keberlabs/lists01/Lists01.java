e9a955fdc2823884e9ae9d65f8ba2f4f904f2f54
Keber <keberflores@gmail.com>
2023-06-18 09:39:18 -0400

    first commit

---
+++ src/main/java/com/keberlabs/lists01/Lists01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.keberlabs.lists01;
+
+/**
+ *
+ * @author Keber
+ */
+public class Lists01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
